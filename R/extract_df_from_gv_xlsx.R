b683780a852ca344a949e196050080eb1d2bc662
Toshihiro Arae <aroearue@gmail.com>
2018-04-10 12:59:03 +0900

    extract_df_from_gv_xlsx

--- R/extract_df_from_gv_xlsx.R
+++ R/extract_df_from_gv_xlsx.R
@@ -14,4 +14,32 @@ extract_df_from_gv_xlsx <-
       {length(.[is.na(.)])}
 
     inf <- read_xlsx(file_path, skip = skip_num + 1, col_names = F)
+    # 1st column: Path from root
+    # 2nd column: Perturbations
+    # 3rd column: Experiment
+    # 4th column: # samples experiment
+    # 5th column: # samples control
+    # 6+(2n)th columns: Log2-ratio
+    # 7+(2n)th columns: p-value
+
+    temp_col <- read_xlsx(file_path, n_max = skip_num + 1, col_names = F)
+    extract_col_info <-
+      function(x){
+        temp_col[x,] %>%
+          as.character %>%
+          .[6:length(.)] %>%
+          replace_na("NA")
+      }
+    colnames(inf) <-
+      c(
+        c("Path_from_root", "Perturbations", "Experiment",
+          "num_samples_experiment", "num_samples_control"),
+        str_c(
+          extract_col_info(skip_num - 1),
+          extract_col_info(skip_num),
+          extract_col_info(skip_num + 1),
+          sep = "_"
+        )
+      )
+    inf
   }
