04de02f7b60a8a81e1fb06103ec4dc23a986001c
Toshihiro Arae <aroearue@gmail.com>
2018-10-16 19:55:04 +0900

    読み込み改善

--- R/extract_df_from_gv_xlsx.R
+++ R/extract_df_from_gv_xlsx.R
@@ -1,45 +1,15 @@
 
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
-
 #' Extract data.frame from Genvestigator .xlsx file
 #' @param file_path .xlsx file path
-#' @importFrom readxl read_xlsx
-#' @importFrom magrittr %>%
 #' @export
 extract_df_from_gv_xlsx <-
   function(file_path){
-    skip_num <-
-      read_xlsx(file_path, col_names = F) %>%
-      .[[1]] %>%
-      {length(.[is.na(.)])}
+    inf <- readxl::read_xlsx(file_path, col_names = F)
 
-    inf <- read_xlsx(file_path, skip = skip_num + 1, col_names = F)
-    # 1st column: Path from root
-    # 2nd column: Perturbations
-    # 3rd column: Experiment
-    # 4th column: # samples experiment
-    # 5th column: # samples control
-    # 6+(2n)th columns: Log2-ratio
-    # 7+(2n)th columns: p-value
-
-    temp_col <- read_xlsx(file_path, n_max = skip_num + 1, col_names = F)
-    extract_col_info <-
-      function(x){
-        temp_col[x,] %>%
-          as.character %>%
-          .[6:length(.)] %>%
-          replace_na("NA")
-      }
-    colnames(inf) <-
-      c(
-        c("Path_from_root", "Perturbations", "Experiment",
-          "num_samples_experiment", "num_samples_control"),
-        str_c(
-          extract_col_info(skip_num - 1),
-          extract_col_info(skip_num),
-          extract_col_info(skip_num + 1),
-          sep = "_"
-        )
-      )
-    inf
+    hoge <- extract_data_df(inf)
+    dplyr::left_join(
+      extract_meta_df(inf),
+      hoge,
+      by = colnames(hoge)[3]
+    )
   }
